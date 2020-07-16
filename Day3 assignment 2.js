5fd5cd8cd071f2ac980ef36de680a852c954a2f8
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-16 07:12:03 +0530

    Add files via upload

---
+++ Day3 assignment 2.js
@@ -0,0 +1,18 @@
+console.log("explain the remaining methods  of string and array");
+console.log("splice");
+console.log ("The splice() method can be used to add new items to an array and delete");
+console.log(" add elements")
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.splice(2, 0, "Lemon","papaya");
+console.log(fruits);
+console.log ("******************************************")
+console.log("replace")
+str = "Please visit Microsoft";
+var n = str.replace("Microsoft", "letsupgrade");
+console.log(n);
+console.log ("******************************************")
+console.log("substring");
+var str = "Hello world!";
+var res = str.substring(1, 4);
+console.log(res);
+console.log ("******************************************")
