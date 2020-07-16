0ba568d78f95af637cf2a2e4da9d65ae73bb1ba9
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-16 07:11:02 +0530

    Add files via upload

---
+++ Day3 assignment 1.js
@@ -0,0 +1,6 @@
+console.log(
+  "write a js code to ask the user to input and log into the console");
+console.log("*********************************************")
+let age =prompt("what is your age");
+console.log(age);
+console.log("*********************************************")
