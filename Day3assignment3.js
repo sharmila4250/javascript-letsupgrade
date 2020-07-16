814cff8aa3d140799c78a8d6ae5876e7e5bdfbb7
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-16 07:12:42 +0530

    Add files via upload

---
+++ Day3assignment3.js
@@ -0,0 +1,6 @@
+console.log("ask the user if he is 21+ log in the value can drink otherwise cannot drink")
+let age=prompt("what is your age");
+console.log(age);
+if(age>21)
+{console.log("can drink");}
+else{console.log("cannot drink");}
