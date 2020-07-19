586061d5a6b212294352ffeafe5669740022118e
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-19 09:27:32 +0530

    Add files via upload

---
+++ Day5 assignment 6.js
@@ -0,0 +1,16 @@
+let num=prompt ("enter no. greater than 100");
+
+
+
+if(num>100)
+{
+  console.log(num);
+}
+else{
+
+  let num=prompt("sorry no.is not greater than 100 so enter again");
+  if(num>100)
+{
+  console.log(num);}
+  else{console.log("");}
+   }
