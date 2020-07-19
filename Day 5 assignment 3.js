a7669bf4c097ddc240ef5f19107372c8a06b126f
sharmila4250 <66352207+sharmila4250@users.noreply.github.com>
2020-07-19 09:25:50 +0530

    Add files via upload

---
+++ Day 5 assignment 3.js
@@ -0,0 +1,4 @@
+let shoppinglist=['chocolate','chips','pulse','deo','ice cream'];
+let shoppingbasket=shoppinglist;
+shoppingbasket.push('cream','sugar','salt','chilly');
+console.log("shopping basket contain"+shoppingbasket);
